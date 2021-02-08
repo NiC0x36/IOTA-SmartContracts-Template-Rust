4d9f634fa18225c0362d9eb26c677b4241ca12d4
Th3B0Y <brunoamancio.ti@gmail.com>
2021-02-08 02:10:21 +0100

    Added multiple code samples

--- Tests/testutils/codesamples/send_and_receive_tokens_test.go
+++ Tests/testutils/codesamples/send_and_receive_tokens_test.go
@@ -7,17 +7,15 @@ import (
 	"github.com/iotaledger/wasp/packages/coretypes"
 	"github.com/iotaledger/wasp/packages/kv/codec"
 	"github.com/iotaledger/wasp/packages/solo"
-	"github.com/iotaledger/wasp/packages/testutil"
 	"github.com/iotaledger/wasp/packages/vm/core/accounts"
+	"github.com/iotaledger/wasp/packages/vm/core/root"
 	"github.com/stretchr/testify/require"
 )
 
 /// This is a sample of how to send tokens from the value tangle to a chain, keeping the ownership of the tokens on that chain
-func Test_SendTokensToChain(t *testing.T) {
+func Test_SendTokensToChain_NoContractFees(t *testing.T) {
 	env := solo.New(t, false, false)
 
-	const initialWalletFunds = testutil.RequestFundsAmount // used to fund address in NewSignatureSchemeWithFunds
-	const transferValueIotas = 1000
 	require.GreaterOrEqual(t, initialWalletFunds, transferValueIotas)
 
 	// Generates key pairs for sender and receiver wallets and provides both with dummy funds.
@@ -42,31 +40,27 @@ func Test_SendTokensToChain(t *testing.T) {
 	chain.PostRequest(transferRequest, senderWalletKeyPair)
 
 	// Wallet balances -> After transfer to chain
-	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialWalletFunds-transferValueIotas-1) // -1 ???
-	chain.AssertAccountBalance(senderAgentID, balance.ColorIOTA, transferValueIotas+1)                        // +1 ???
+	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialWalletFunds-transferValueIotas-iotaTokensConsumedByRequest)
+	chain.AssertAccountBalance(senderAgentID, balance.ColorIOTA, transferValueIotas+iotaTokensConsumedByRequest)
 }
 
-func Test_SendAndReceiveTokens(t *testing.T) {
+func Test_SendAndReceiveTokens_NoContractFees(t *testing.T) {
 	env := solo.New(t, false, false)
 
-	const initialSenderWalletFunds = testutil.RequestFundsAmount // used to fund address in NewSignatureSchemeWithFunds
-	const transferValueIotas = 1000
-	require.GreaterOrEqual(t, initialSenderWalletFunds, transferValueIotas)
+	require.GreaterOrEqual(t, initialWalletFunds, transferValueIotas)
 
 	// Generates key pairs for sender wallet and provides it with dummy funds.
 	senderWalletKeyPair := env.NewSignatureSchemeWithFunds()
 	senderWalletAddress := senderWalletKeyPair.Address()
 	senderWalletAgentID := coretypes.NewAgentIDFromAddress(senderWalletAddress)
-	senderIotaBalance := env.GetAddressBalance(senderWalletAddress, balance.ColorIOTA)
 	require.NotNil(t, senderWalletKeyPair)
 	require.NotNil(t, senderWalletAddress)
-	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialSenderWalletFunds)
+	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialWalletFunds)
 
 	// Generates key pairs for sender wallet.
 	receivedWalletKeyPair := env.NewSignatureScheme()
 	receiverWalletAddress := receivedWalletKeyPair.Address()
 	receiverWalletAgentID := coretypes.NewAgentIDFromAddress(receiverWalletAddress)
-	receiverIotaBalance := env.GetAddressBalance(receiverWalletAddress, balance.ColorIOTA)
 	require.NotNil(t, receivedWalletKeyPair)
 	require.NotNil(t, receiverWalletAddress)
 	require.NotNil(t, receiverWalletAgentID)
@@ -76,19 +70,57 @@ func Test_SendAndReceiveTokens(t *testing.T) {
 	chain := env.NewChain(nil, "myChain")
 
 	// Transfer within the chain
-	params := codec.MakeDict(map[string]interface{}{
-		accounts.ParamAgentID: codec.EncodeAgentID(receiverWalletAgentID),
-	})
-	transferRequest := solo.NewCallParamsFromDic(accounts.Name, accounts.FuncDeposit, params).WithTransfer(balance.ColorIOTA, transferValueIotas)
+	transferRequest := solo.NewCallParams(accounts.Name, accounts.FuncDeposit, accounts.ParamAgentID, codec.EncodeAgentID(receiverWalletAgentID)).
+		WithTransfer(balance.ColorIOTA, transferValueIotas)
+
 	chain.PostRequest(transferRequest, senderWalletKeyPair)
 
 	// Wallet balances -> After transfer
-	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialSenderWalletFunds-transferValueIotas-1) // -1 ???
-	chain.AssertAccountBalance(senderWalletAgentID, balance.ColorIOTA, +1)                                          // +1 ???
+	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialWalletFunds-transferValueIotas-iotaTokensConsumedByRequest)
+	chain.AssertAccountBalance(senderWalletAgentID, balance.ColorIOTA, iotaTokensConsumedByRequest)
 
 	env.AssertAddressBalance(receiverWalletAddress, balance.ColorIOTA, 0)
-	chain.AssertAccountBalance(receiverWalletAgentID, balance.ColorIOTA, 0+transferValueIotas)
+	chain.AssertAccountBalance(receiverWalletAgentID, balance.ColorIOTA, transferValueIotas)
+}
 
-	t.Logf("Sender wallet address after transfer: %s - Balance: %d IOTA(s)", senderWalletAddress, senderIotaBalance)
-	t.Logf("Receiver wallet address after transfer: %s - Balance: %d IOTA(s)", receiverWalletAddress, receiverIotaBalance)
+func Test_SendTokensToChain_WithContractFees(t *testing.T) {
+	env := solo.New(t, false, false)
+
+	require.GreaterOrEqual(t, initialWalletFunds, transferValueIotas)
+
+	// Generates key pairs for sender and receiver wallets and provides both with dummy funds.
+	senderWalletKeyPair := env.NewSignatureSchemeWithFunds()
+	senderWalletAddress := senderWalletKeyPair.Address()
+	senderAgentID := coretypes.NewAgentIDFromAddress(senderWalletAddress)
+	require.NotNil(t, senderWalletKeyPair)
+	require.NotNil(t, senderWalletAddress)
+	require.NotNil(t, senderAgentID)
+
+	// Wallet balance in value tangle -> Before transfer
+	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialWalletFunds)
+
+	// Generate a dummy chain NOT beloging to sender
+	chain := env.NewChain(nil, "myChain")
+	feeColor, ownerFee, validatorFee := chain.GetFeeInfo(accounts.Name)
+	require.Equal(t, balance.ColorIOTA, feeColor)
+	require.Equal(t, int64(0), ownerFee)
+	require.Equal(t, int64(0), validatorFee)
+
+	// Request to chain change fee settings
+	const newOwnerFee = int64(100)
+	transferRequest := solo.NewCallParams(root.Interface.Name, root.FuncSetContractFee, root.ParamHname, accounts.Interface.Hname(), root.ParamOwnerFee, newOwnerFee)
+	_, err := chain.PostRequest(transferRequest, chain.OriginatorSigScheme)
+	require.NoError(t, err)
+
+	// Wallet balance in chain -> Before transfer
+	chain.AssertAccountBalance(senderAgentID, balance.ColorIOTA, 0)
+
+	// Transfer from value tangle to the chain
+	transferRequest = solo.NewCallParams(accounts.Name, accounts.FuncDeposit).WithTransfer(balance.ColorIOTA, transferValueIotas)
+	_, err = chain.PostRequest(transferRequest, senderWalletKeyPair)
+	require.NoError(t, err)
+
+	// Wallet balances -> After transfer to chain
+	env.AssertAddressBalance(senderWalletAddress, balance.ColorIOTA, initialWalletFunds-transferValueIotas-iotaTokensConsumedByRequest) // Transfered tokens are debited from the value tangle
+	chain.AssertAccountBalance(senderAgentID, balance.ColorIOTA, transferValueIotas+iotaTokensConsumedByRequest-newOwnerFee)            // His tokens in the chain minus fees
 }
